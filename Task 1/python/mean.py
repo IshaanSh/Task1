b09bbaeee3b3c7290e7f9234c06e846c80d4e582
IshaanSh <68664036+IshaanSh@users.noreply.github.com>
2021-07-02 20:31:46 +0530

    Delete Task 1 directory

--- Task 1/python/mean.py
+++
@@ -1,7 +0,0 @@
-n_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-n = len(n_num)
-  
-get_sum = sum(n_num)
-mean = get_sum / n
-  
-print("Mean / Average is: " + str(mean))
